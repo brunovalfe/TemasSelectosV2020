576c2481620b7d5dd7fd41f31bacbf40ab98530b
bvideal <brunoavalfe@gmail.com>
2020-06-18 11:22:51 -0500

    to master

---
+++ ProyectoTemaS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectotemas;
+
+/**
+ *
+ * @author Tipolinapuchi
+ */
+public class ProyectoTemaS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      LoginFrame window = new LoginFrame();
+        window.setTitle("BubbApp");
+        window.setLocationRelativeTo(null);//Para mandar la ventana al centro
+        window.setVisible(true);
+    }
+    
+}
